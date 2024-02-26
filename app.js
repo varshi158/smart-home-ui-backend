9d17463bb9c2471db8810adc4e559441f40db51d
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-02-26 09:25:38 +0530

    error handling and put (update) method

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const devices = [];
 
 // getting all the devices
 app.get("/devices", (req, res) => {
-  return res.status(200).json(devices);
+  try {
+    return res.status(200).json(devices);
+  }
+  catch (error) {
+      console.log(error);
+      return res.status(500).send();
+  }
 });
 
 // getting a single device
@@ -59,6 +65,25 @@ app.delete("/devices/:id", (req, res) => {
       
       devices.splice(deviceIndex, 1); 
       return res.status(204).send(); 
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+// update a device
+app.put("/devices/:id", (req, res) => {
+  try {
+    const device = req.body;
+    const found = devices.find((el) => el.id === parseInt(id));
+    if(!found) {
+      return res.status(404).send();
+    }
+    found.name = device.name;
+    found.state = device.state;
+    found.location = device.location;;
+    return res.status(201).send();
   }
   catch (error) {
     console.log(error);
