4a06493e440083f6ce44d1296b3009168be929dc
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-05-29 16:06:43 +0530

    feat: full db crud

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,7 +1,9 @@
+import Device  from "../models/Device.js";
 const devices = [];
 
-export const getAllDevices = (req, res) => {
+export const getAllDevices = async (req, res) => {
     try {
+      const devices = await Device.find();
       return res.status(200).json(devices);
     }
     catch (error) {
@@ -10,14 +12,19 @@ export const getAllDevices = (req, res) => {
     }
   };
 
-export const getDevice = (req, res) => {
+export const getDevice = async (req, res) => {
 
     try{
       const {id} = req.params;
-      const device = devices.find( (el) => el.id === parseInt(id));
+      //const device = devices.find( (el) => el.id === parseInt(id));
     
+      // if (!device) {
+      //   return res.status(404).send();
+      // }
+
+      const device = await Device.findById(id);
       if (!device) {
-        return res.status(404).send();
+        return res.status(404).json(device);
       }
       return res.status(200).json(device);
     }
@@ -28,15 +35,16 @@ export const getDevice = (req, res) => {
   };
 
 
-export const createDevice = (req, res) => {
+export const createDevice =  async (req, res) => {
 
     try {
         const device = req.body;
-        const found = devices.find((el) => el.id === parseInt(device.id));
-        if (found) {
-            return res.status(400).send();
-        }
-        devices.push(device);
+        // const found = devices.find((el) => el.id === parseInt(device.id));
+        // if (found) {
+        //     return res.status(400).send();
+        // }
+        // devices.push(device);
+        await Device.create(device);
         return res.status(201).send();
     }
     catch(error) {
@@ -45,17 +53,17 @@ export const createDevice = (req, res) => {
     }
   };
   
-export const deleteDevice =  (req, res) => {
+export const deleteDevice = async (req, res) => {
     try {
       const { id } = req.params;
   
-      const deviceIndex = devices.findIndex( (el) => el.id === parseInt(id));
+      // const deviceIndex = devices.findIndex( (el) => el.id === parseInt(id));
       
-        if (deviceIndex === -1) {
-          return res.status(404).send();
-        }
-        
-        devices.splice(deviceIndex, 1); 
+      //   if (deviceIndex === -1) {
+      //     return res.status(404).send();
+      //   }
+        await Device.findByIdAndDelete(id);
+        //devices.splice(deviceIndex, 1); 
         return res.status(204).send(); 
     }
     catch (error) {
@@ -64,18 +72,21 @@ export const deleteDevice =  (req, res) => {
     }
   };
 
-export const fullyUpdateDevice = (req, res) => {
+export const fullyUpdateDevice = async (req, res) => {
     try {
       const { id } = req.params;
   
       const device = req.body;
-      const found = devices.find((el) => el.id === parseInt(id));
+      //const found = devices.find((el) => el.id === parseInt(id));
+      const found = Device.findById(id);
       if(!found) {
         return res.status(404).send();
       }
-      found.name = device.name;
-      found.state = device.state;
-      found.location = device.location;
+      // found.name = device.name;
+      // found.state = device.state;
+      // found.location = device.location;
+
+      const fullyUpdatedDevice = await Device.findByIdAndUpdate(id, {name: device.name, state: device.state, image: device.image, location: device.location});
       return res.status(201).send();
     }
     catch (error) {
@@ -84,17 +95,21 @@ export const fullyUpdateDevice = (req, res) => {
     }
   };
 
-export const partiallyUpdateDevice = (req, res) => {
+export const partiallyUpdateDevice = async(req, res) => {
     try {
       const { id } = req.params;
   
       const data = req.body;
-      const found = devices.find((el) => el.id === parseInt(id));
+      //const found = devices.find((el) => el.id === parseInt(id));
+      const found = Device.findById(id);
       if(!found) {
         return res.status(404).send();
       }
       
-      found.state = data.state;
+      //found.state = data.state;
+      const partiallyUpdatedDevice = await Device.findByIdAndUpdate(id, {
+        state: data.state,
+      });
       return res.status(201).send();
     }
     catch (error) {
