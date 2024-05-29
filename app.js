70e0ebc947ed804bf9814d77157b2729be5bcf8f
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-05-29 12:40:28 +0530

    establish database connection

--- app.js
+++ app.js
@@ -2,15 +2,43 @@
 
 import express from "express";
 import deviceRouter from "./routes/deviceRouter.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-// web service called app
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an express application - web service called app
 const app = express();
 app.use(express.json());
 
 // middleware - lies between the request and response
+// Use the deviceRouter for routes starting with /api/devices
 app.use("/api/devices", deviceRouter);
 
-const PORT = 8000;
+
+//const PORT = 8000;
+
+// connecting the mongodb database
+// const DB = process.env.DATABASE_URL;
+// mongoose.connect(DB).then(() => {
+//   console.log("db connection successful");
+// })
+// .catch((error) => {
+//   console.log(error);
+// })
+
+const PORT = process.env.PORT || 8000;
+
+// Connect to MongoDB
+const DB = process.env.DATABASE_URL;
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
